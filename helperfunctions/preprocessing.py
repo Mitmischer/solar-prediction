2fd4edcebe6f188688fbff5ae14d260a54538e3f
Anna-Valentina Hirsch <a-valentina.hirsch@hotmail.com>
2024-06-19 01:00:31 +0200

    added notebook for cleaning and preprocessing

--- helperfunctions/preprocessing.py
+++ helperfunctions/preprocessing.py
@@ -1,27 +1,62 @@
 import pandas as pd
 from statsmodels.tsa.stattools import adfuller
 
-def adjust_dayofyear(row: pd.Series, date_col: str) -> pd.Series:
+
+# split into train val test
+def split_ts(ts: pd.Series, val_start: str, test_start: str, date_column: str) -> tuple[pd.Series, pd.Series, pd.Series]:
+    """
+    Split a time series into training, validation, and test sets based on the specified dates.
+    Set the datetime column as the index for each set.
+
+    Parameters:
+    ts (pd.Series): Time series data with a datetime index.
+    val_start (str): The start date for the validation set.
+    test_start (str): The start date for the test set.
+    date_column (str): The name of the column containing datetime values.
+
+    Returns:
+    pd.Series: The training, validation, and test sets.
+    """
+    train = ts[ts[date_column] < val_start]
+    val = ts[(ts[date_column] >= val_start) & (ts[date_column] < test_start)]
+    test = ts[ts[date_column] >= test_start]
+
+    # Set indices
+    train_ts = train.set_index(keys=date_column, drop=True)
+    val_ts = val.set_index(keys=date_column, drop=True)
+    test_ts = test.set_index(keys=date_column, drop=True)
+
+    return train_ts, val_ts, test_ts
+
+
+# handle leap years
+def handle_leap_years(ts: pd.DataFrame) -> pd.DataFrame:
     """
     Adjust the day of the year for leap years and extract the month from the date.
 
     Parameters:
-    row (pd.Series): A row of data containing a date column.
-    date_col (str): The name of the column containing datetime values.
+    ts (pd.DataFrame): The DataFrame containing time series data with datetime index.
 
     Returns:
-    pd.Series: A series with adjusted day of the year and month.
-
-    Explanation:
-    The adjustment ensures that the day of the year after February 28th in leap years
-    is consistent with non-leap years by subtracting 1 from the day of the year
-    for dates after February 28th. This removes the extra day (February 29th) from the count.
+    pd.DataFrame: A DataFrame with the original data and additional columns
+                  for adjusted day of the year and month.
     """
-    date = row[date_col]
-    adjusted_day = date.dayofyear
-    if date.is_leap_year and date.dayofyear > 59:  # Check for leap year and adjust the day of the year
-        adjusted_day -= 1
-    return pd.Series({"adjusted_dayofyear": adjusted_day, "month": date.month})
+
+    def remove_leapyears(row: pd.Series) -> pd.Series:
+        date = row.name
+        adjusted_day = date.dayofyear
+        if date.is_leap_year and date.dayofyear > 59:
+            adjusted_day -= 1
+        return pd.Series({"adjusted_dayofyear": adjusted_day, "month": date.month})
+
+    # Apply the function to the DataFrame
+    new_columns = ts.apply(remove_leapyears, axis=1)
+
+    # Add the new columns to the DataFrame
+    ts["adjusted_dayofyear"] = new_columns["adjusted_dayofyear"]
+    ts["month"] = new_columns["month"]
+
+    return ts
 
 
 # Check for continuity
@@ -40,14 +75,31 @@ def check_continuity(ts: pd.Series, split: str, freq: str = '15min') -> None:
     expected_range = pd.date_range(start=ts.index.min(), end=ts.index.max(), freq=freq)
     missing_times = expected_range.difference(ts.index)
     if not missing_times.empty:
-        print(f"Missing timestamps detected in {split}: {missing_times}")
+        print(f"Missing timestamps detected in {split} dataset:")
+        print(f"{missing_times}")
     else:
-        print(f"No missing timestamps in {split}")
+        print(f"No missing timestamps in {split} dataset")
+
+
+# Interpolate missing timestamps
+def interpolate_missing_timestamps(ts: pd.Series, freq: str = '15min') -> pd.Series:
+    """
+    Interpolate missing timestamps in a time series data based on a specified frequency.
+
+    Parameters:
+    ts (pd.Series): Time series data with a datetime index.
+    freq (str): Frequency string (default is '15min').
+
+    Returns:
+    pd.Series: Time series data with missing timestamps interpolated.
+    """
+    expected_range = pd.date_range(start=ts.index.min(), end=ts.index.max(), freq=freq)
+    interpolated = ts.reindex(expected_range)
+    interpolated.interpolate(method='time', inplace=True)
+    return interpolated
 
 
 # Resample
-import pandas as pd
-
 def resample_ts(ts: pd.Series, freq: str = 'h') -> pd.Series:
     """
     Resample a time series to a specified frequency and aggregate the values by summing.
@@ -83,7 +135,7 @@ def normalise_ts(train_ts, val_ts, test_ts: pd.Series) -> pd.Series:
     return normalised_train, normalised_val, normalised_test
 
 
-# Stationarity - Augmented Dicky Fuller Test
+# Make stationary - Augmented Dicky Fuller Test
 def make_stationary(train_ts, val_ts, test_ts: pd.Series) -> None:
     """
     Perform the Augmented Dickey-Fuller test on train_ts. If train_ts is not stationary,
@@ -131,5 +183,3 @@ def make_stationary(train_ts, val_ts, test_ts: pd.Series) -> None:
             val_ts[col] = val_ts[col].diff().dropna()
             test_ts[col] = test_ts[col].diff().dropna()
         print("\n")  # Add a space between outputs
-
-
