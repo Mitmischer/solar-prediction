154b155eb5e36407bb20a8b07a4f3425e2ca103e
moonchild <a-valentina.hirsch@hotmail.com>
2024-07-29 02:26:34 +0200

    add import to init file

---
+++ modellib/__init__.py
@@ -0,0 +1,10 @@
+from . import train
+from . import backtest
+from . import lstm
+from . import dataloader
+from . import evaluate
+from . import io
+from . import modelclass
+from . import models
+from . import moving_average
+
